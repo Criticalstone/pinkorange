fe7941879c05f48433bf3316b8af713790063cce
Criticalstone <kevinhoog@gmail.com>
2016-10-19 15:03:58 +0200

    yes

--- js/easter.js
+++ js/easter.js
@@ -1,11 +1,30 @@
 (function($){
   $(function(){
+    $("#w").click(function() {
+      animateForever();
+    });
     $("#s").click(function() {
-      $("#orange").toggleClass("amber-text");
-      $("#orange").toggleClass("pink-text text-lighten-2");
-      $("#pink").toggleClass("amber-text");
-      $("#pink").toggleClass("pink-text text-lighten-2");
-      $("#pinkorange").effect("bounce", 500);
+      animateOnce();
     });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function animateForever() {
+  changeColors();
+  $("#pinkorange").effect("bounce", 500).delay(300).queue(function(){
+    $(this).dequeue();
+    animateForever();
+  });
+}
+
+function changeColors() {
+  $("#orange").toggleClass("amber-text");
+  $("#orange").toggleClass("pink-text text-lighten-2");
+  $("#pink").toggleClass("amber-text");
+  $("#pink").toggleClass("pink-text text-lighten-2");
+}
+
+function animateOnce() {
+  changeColors();
+  $("#pinkorange").effect("bounce", 500);
+}
